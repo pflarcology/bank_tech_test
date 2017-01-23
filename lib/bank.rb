646717c9bfc85dca3b05b5bfb4e8da8710e13774
pflarcology <rhrg500@gmail.com>
2017-01-23 10:23:48 +0000

    adds method deposit_money

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,9 +1,12 @@
 class Bank
 
+  attr_reader :balance
+
   def initialize
     @balance = 0
   end
 
-  attr_reader :balance
+  def deposit_money
+  end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,12 +2,16 @@ require 'bank'
 
 describe Bank do
 
-  it 'balance' do
+  it 'responds to the method balance' do
     expect(subject).to respond_to :balance
   end
 
   it 'initialises with 0' do
     expect(subject.balance).to eq 0
+  end
+
+  it 'responds to the method depost money' do
+    expect(subject).to respond_to :deposit_money
   end
 
 end
